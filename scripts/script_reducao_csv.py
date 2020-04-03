951ebbab811e45806d8b8d666f22e867d1e5e2f5
marcos314 <marcoswesley314@gmail.com>
2020-04-02 23:56:58 -0300

    copia csv - ok

--- scripts/script_reducao_csv.py
+++ scripts/script_reducao_csv.py
@@ -3,23 +3,24 @@ import csv
 
 #df = pd.read_csv('/home/marcos/Desktop/teste_1.csv')
 
-with open('/home/marcos/Desktop/dados_teste/teste_1.csv', "r") as file:
+with open('/home/marcos/Desktop/dados_teste/teste_escrita_1.csv', "r+") as file_w:
+    
+    with open('/home/marcos/Desktop/dados_teste/teste_1.csv', "r") as file:
+        
+        reader = csv.reader(file)
+        head = next(reader)  
+        data = []
+        writer = csv.writer(file_w)
+        writer.writerow(head)
+        for row in reader:
+            data = row
+            print(row)
+        
+            writer = csv.writer(file_w)
+            writer.writerow(data)
 
-    reader = csv.reader(file, delimiter = ";")
+        
 
-    head = next(reader)
-
-    print(head)
-
-    for row in reader:
-
-        print(row)
-
-
-with open('/home/marcos/Desktop/dados_teste/teste_escrita_1.csv', "w") as file_w:
-
-    writer = csv.writer(file_w)
-    writer.writerow(head)
 
 
 
