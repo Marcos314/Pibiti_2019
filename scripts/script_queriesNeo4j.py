af34382e3b27b4c0f96725978cb147099b1d4c5f
marcos314 <marcoswesley314@gmail.com>
2020-04-07 22:30:57 -0300

    queriesNeo4j

--- scripts/script_queriesNeo4j.py
+++ scripts/script_queriesNeo4j.py
@@ -19,15 +19,16 @@ class BolsaFamilia:
 
         query = "LOAD CSV WITH HEADERS FROM 'file:///teste3.csv' as line CREATE  (p:Pagamento {mesReferencia: line.MES_REFERENCIA, mesCompetencia: line.MES_COMPETENCIA, nomeMunicipio: line.NOME_MUNICIPIO, valorParcela: line.VALOR_PARCELA,codigoMunicipio: line.CODIGO_MUNICIPIO_SIAFI, uf: line.UF})"
 
-        query2 = "LOAD CSV WITH HEADERS FROM 'file:///teste3.csv' as line MERGE (b:Beneficiario {nome: line.NOME_FAVORECIDO, nis: line.NIS_FAVORECIDO})"
+        query2 = "LOAD CSV WITH HEADERS FROM 'file:///teste3.csv' as line CREATE (b:Beneficiario {nome: line.NOME_FAVORECIDO, nis: line.NIS_FAVORECIDO})"
 
-        relation = "LOAD CSV WITH HEADERS FROM 'file:///teste3.csv' as line MATCH (b:Beneficiario {nome: line.NOME_FAVORECIDO, nis: line.NIS_FAVORECIDO}), (p:Pagamento {nomeMunicipio: line.NOME_MUNICIPIO,  uf: line.UF}) CREATE (b)-[:RECEBEU]->(p)"
+        query3 = "LOAD CSV WITH HEADERS FROM 'file:///teste3.csv' as line MERGE (b:Beneficiario {nis: line.NIS_FAVORECIDO})"
 
-
+        relation = "LOAD CSV WITH HEADERS FROM 'file:///teste3.csv' as line MATCH (b:Beneficiario) MERGE (p:Pagamento {nomeMunicipio: line.NOME_MUNICIPIO,  uf: line.UF}) MERGE (b)-[:RECEBEU]->(p)"    
 
 
         self.GRAPH.run(query)
         self.GRAPH.run(query2)
+        self.GRAPH.run(query3)
         self.GRAPH.run(relation)
 
 
