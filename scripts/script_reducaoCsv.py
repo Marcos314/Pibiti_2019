668ebd7c1fc5a0f58bb12025c1b5b7aeb190677e
marcos314 <marcoswesley314@gmail.com>
2020-04-08 21:10:30 -0300

    update

--- scripts/script_reducaoCsv.py
+++ scripts/script_reducaoCsv.py
@@ -6,16 +6,32 @@ limInsercao = 10**4
 ten = 10
 count = 10**2
 
+
+def findUF():
+
+    dataframe = pd.read_csv('/home/marcos/Desktop/TCC_2020/dados/201501.csv')        
+    lista = []
+    count = 0
+    for index in dataframe.iterrows():
+        if dataframe.loc(dataframe['UF'] == 'DF'):
+        #lista.append(dataframe[''])
+            lista.append(dataframe.loc(dataframe['NOME_MUNICIPIO']))
+        count+=1
+        if count >10:
+            break    
+    
+    return lista
 #df = pd.read_csv('/home/marcos/Desktop/teste_1.csv')
 
-with open('/home/marcos/Desktop/dados_teste/testeW1.csv', "w") as file_w:
+with open('/home/marcos/Desktop/dados_teste/testeW2.csv', "w") as file_w:
 #with open('/home/marcos/neo4j/neo4j-pibiti/teste_escrita_1.csv', "w") as file_w:
-
     with open('/home/marcos/Desktop/TCC_2020/dados/201501.csv', "r") as file:
         
         reader = csv.reader(file)
         head = next(reader)
         reader = csv.reader(file)
+
+
         writer = csv.writer(file_w)
         writer.writerow(head)
         data = []   
@@ -36,7 +52,9 @@ with open('/home/marcos/Desktop/dados_teste/testeW1.csv', "w") as file_w:
        
             else:
                 print(f"INSERIDO NO TOTAL: {bigCount} REGISTROS")
+                
                 break
+
 
         
 
--- scripts/teste.py
+++ scripts/teste.py
@@ -1,14 +1,30 @@
-a = (10**6) + 5
-b= 10**5
+import csv
+import pandas as pd
 
-c = a//b
+with open('/home/marcos/Desktop/dados_teste/testeW2.csv', "w") as file_w:
 
-d =  (c % 2)
+    with open('/home/marcos/Desktop/TCC_2020/dados/201501.csv', "r") as file:
+        
+        reader = csv.reader(file)
+        head = next(reader)
 
-print(f"d= {d}")
+        reader = csv.reader(file)
 
-print(c)
-print(type(c))
+        writer = csv.writer(file_w)
+        writer.writerow(head)  
 
+        df = pd.read_csv('/home/marcos/Desktop/TCC_2020/dados/201501.csv', low_memory=False)       
 
-    
+        bigCount = 0
+        limInsercao = 10**3
+        ten = 10
+        count = 10
+        lista = []
+        for row  in reader:
+
+            if df.columns[2] == 'UF':
+                lista = row                
+                writer = csv.writer(file_w)
+                writer.writerow(lista)
+
+        print(lista)
