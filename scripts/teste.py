43703f7dcea8548c652865fd625230b220649a57
marcos314 <marcoswesley314@gmail.com>
2020-04-09 22:35:09 -0300

    Filtrando csv por UF

--- scripts/teste.py
+++ scripts/teste.py
@@ -1,3 +1,6 @@
+
+'''FUNÇÃO PARA FILTRAR CSV POR UF'''
+
 import pandas as pd
 
 
@@ -9,7 +12,7 @@ df2 = pd.read_csv(filename, sep=',')
 
 
 df.query('UF == "DF"').to_csv('/home/marcos/Desktop/dados_teste/testeW3.csv', index=False)
-df2.query('UF == "GO"').to_csv('/home/marcos/Desktop/dados_teste/testeW3.csv', mode='a', index=False)
+df2.query('UF == "GO"').to_csv('/home/marcos/Desktop/dados_teste/testeW3.csv', mode='a', index=False) #mode 'a' não exclui os dados existentes
 
 print(df.query('UF == "DF"'))
 
