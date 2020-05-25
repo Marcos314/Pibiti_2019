921aaa756eaa45c6b11d5afe6a03bd94d686f124
marcos314 <marcoswesley314@gmail.com>
2020-05-25 10:02:55 -0300

    Fix: update dashboard app

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,54 +1,48 @@
+# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.express as px
-
-df = pd.read_csv('/home/marcos/Desktop/df_teste.csv')
-
-
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(max(len(dataframe), max_rows))
-        ])
-    ])
-
-############################ PLOTAGEM DE GRÁFICOS #############################
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-fig = px.bar(df,x='ANO',y='VALORES')
+colors = {
+    'background': '#999',
+    'text': '#11111'
+}
 
-app.layout = html.Div(children=[
-
-    html.H2(children='Dashboard'),
-
-    html.Div([    
-    dcc.Graph(
-        figure=fig,
-        'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        
-        
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
     ),
-    
-    html.H4(children='DF TESTE'),
-    generate_table(df)
-    
-    
 
-    ])
-    
+    html.Div(children='Dash: A web application framework for Python.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    )
 ])
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
