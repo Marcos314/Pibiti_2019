7b4696a54a74b3839b9b79a6b23d414d541c0fac
marcos314 <marcoswesley314@gmail.com>
2020-08-12 08:49:04 -0300

    Nova atualização - organização dos arquivos

--- scripts/pipeline2.py
+++ scripts/pipeline2.py
@@ -87,7 +87,7 @@ def filterUf():
         #df.query('UF == "DF"').to_csv(f'/home/marcos/Desktop/dados_teste/clean_data/{file}', index=False)
         #df2.query('UF == "GO"').to_csv('/home/marcos/Desktop/dados_teste/201301_ok.csv', mode='a', index=False) #mode 'a' não exclui os dados existentes
         print('RIDE\n')
-        df.query('CODIGO_MUNICIPIO_SIAFI in ["9645","9645","9771","1052","9205","9211","9215","9263","9279","5407","0578","4185","4089","1068","0067","1066","9305","9755","9597","9677","9595","9509","1058","9931","9445","9361","9371","9445","0077","0055","9317","9359","9325","9543","9489","9701"]').to_csv(f"/home/marcos/Desktop/dados/clean_data/{file}", mode='a', index=False) #mode 'a' não exclui os dados existentes
+        df.query('CODIGO_MUNICIPIO_SIAFI in ["9645","9771","1052","9205","9211","9215","9263","9279","5407","0578","4185","4089","1068","0067","1066","9305","9755","9597","9677","9595","9509","1058","9931","9445","9361","9371","9445","0077","0055","9317","9359","9325","9543","9489","9701"]').to_csv(f"/home/marcos/Desktop/dados/clean_data/{file}", mode='a', index=False) #mode 'a' não exclui os dados existentes
 
     spend = time.time() - start_time
     hour = spend//3600
